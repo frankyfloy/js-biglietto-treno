c046fd962fea3ec5dd4fa7fbc274bc93e33a664f
Francesco Sica <fsica171@gmail.com>
2021-03-01 15:02:46 +0100

    Inizio parte logica

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+// Dichhiarazione e inizializzazione variabili
+var km = prompt("Inserisci i km da percorrere : ");
+var età = prompt("Inserisci la tua età : ");
+var scontoMinori = 0.20;
+var scontoOver65 = 0.40;
+var prezzoKm = 0.21;
+var prezzoTotale;
+
+
+// Calcolo prezzo non scontato
+prezzo = prezzoKm  * km;
+
+console.log(prezzo);
