8d6ac82b46a479e869d3b6c0951d214aa37348bb
Francesco Sica <fsica171@gmail.com>
2021-03-01 16:40:06 +0100

    Creazione pagina

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,7 @@
 //inizializzazione variabili
+var nome;
+var cognome;
+
 var km = 0;
 var età = -1;
 var scontoMinori = 20;
@@ -7,10 +10,20 @@ var prezzoKm = 0.21;
 var prezzoTotale = 0;
 
 
+
+// INPUT UTENTE------------------------------------------
+var nome = prompt("Inserisci il tuo nome : ");
+document.getElementById('nome').innerHTML = nome;
+
+
+var cognome = prompt("Inserisci il tuo cognome : ");
+document.getElementById('cognome').innerHTML = cognome;
+
 // Controllo Input km
 var kmTemp = prompt("Inserisci i km da percorrere : ");
 if (!isNaN(kmTemp)) {
     km = kmTemp;
+    document.getElementById('km').innerHTML = km;
 }else {
     console.log("Kilometri non validi");
 }
@@ -20,13 +33,17 @@ if (!isNaN(kmTemp)) {
 var etàTemp = prompt("Inserisci la tua età : ");
 if (!isNaN(etàTemp)) {
     età = etàTemp;
+    document.getElementById('età').innerHTML = età;
 }else {
     console.log("Età non valida");
 }
+// ---------------------FINE INPUT-------------------------
 
 
 
-// Calcolo sconto
+
+
+// Calcolo prezzo
 if (!isNaN(km)  && !isNaN(età)) {
     prezzoTotale = prezzoKm * km;
 
@@ -40,4 +57,5 @@ if (!isNaN(km)  && !isNaN(età)) {
         prezzoTotale;
         console.log(prezzoTotale);
     }
+    document.getElementById('prezzoBiglietto').innerHTML = prezzoTotale;
 }
